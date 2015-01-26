f55a03560f1fd9a03ba82d9d99b4c8689ff6cbce
Taihwan Hah <duecorda@gmail.com>
2015-01-26 21:51:26 +0900

    유니콘 재시작 스크립트

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,14 +17,22 @@ set :ssh_options, { forward_agent: true }
 
 namespace :deploy do
 
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      execute "./bin/init.unicorn.rb restart"
+    end
+  end
+  after :publishing, :restart
+
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
       #   execute :rake, 'cache:clear'
       # end
-      execute "./bin/init.unicorn.rb restart"
     end
   end
 
 end
+
--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,10 +2,8 @@ role :app, %w{deployer@58.96.185.60}
 role :web, %w{deployer@58.96.185.60}
 role :db,  %w{deployer@58.96.185.60}
 
-server '58.96.185.60', user: 'deployer', roles: %w{web app db}, app_hosts: %w{127.0.0.1}, db_host: '127.0.0.1', unicorn_workers: 4
+server '58.96.185.60', user: 'deployer', roles: %w{web app db}
 
-set :nginx_use_ssl, true # SSL 사용함
-set :nginx_server_name, "web" # crt/key 파일 이름으로 사용됨
-set :nginx_upload_certificate, true # crt/key 업로드: config/resources에 web.crt, web.key의 인증서 파일2개가 존재해야함
-
-set :unicorn_workers, 4
+# set :nginx_use_ssl, true # SSL 사용함
+# set :nginx_server_name, "web" # crt/key 파일 이름으로 사용됨
+# set :nginx_upload_certificate, true # crt/key 업로드: config/resources에 web.crt, web.key의 인증서 파일2개가 존재해야함
